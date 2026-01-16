9c4a2656101666238f02657b84a59b1ea52ad2e9
DESKTOP-7BF8T50\user1 <wldghs5s@gmail.com>
2026-01-16 17:43:44 +0900

    카드뒤집기겜

--- app/index.js
+++ app/index.js
@@ -1,1 +1,63 @@
-console.log("zzz");
+function setListenerToCard(){
+
+    const cardAreaArr = document.querySelectorAll(".card-area");
+    
+    for(const cardArea of cardAreaArr){
+        cardArea.addEventListener("click" , function(evt){
+            const temp = evt.currentTarget;
+            temp.classList.toggle("flip");
+            
+            setTimeout(() => {
+     
+            temp.classList.toggle("flip");
+            } , 3000 );
+        });
+    }
+}
+
+
+const main = document.querySelector("main");
+
+function generateCardList(){
+    const cardCnt = document.querySelector("#cardCnt").value;
+    if(cardCnt > 50){
+        alert("최대 50개 ,,,개발자 괴롭히기를 멈춰주세요 ,,")
+        return;
+    }
+    main.innerHTML = "";
+
+    const cardContentArr = [];
+    for(let i = 1; i<= cardCnt; i++){
+        cardContentArr.push(i);
+    }
+    const arr = cardContentArr.concat(cardContentArr);
+
+    const result = shuffleArr(arr);
+    for(temp of arr){
+        main.innerHTML += `
+            <div class="card-area">
+                    <div class="card">
+                    <div class="card-back">${temp}</div>
+                    <div class="card-front">?</div>
+                </div>
+            </div>
+`;
+    }
+}
+
+function shuffleArr(arr){
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  return arr;
+}
+
+function handleClick(){
+    generateCardList();
+    setListenerToCard();
+}
+
+
+
+
